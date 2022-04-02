855c623b4963a8116ec15de12c4c043271289e6a
Pacifist <desmondmorales92@gmail.com>
2022-04-02 19:23:12 +0300

    reworking conditions

--- script.js
+++ script.js
@@ -2,10 +2,26 @@ let num = +prompt("Введите число")
 let i
 let count
 
+// if (num < 1) {
+//     console.log("NaN")
+// } else if (num === 1) {
+//     console.log("Число простое")
+// } else {
+//     for (i = 2; i <= num; i++) {
+//         for (count = 2; count < num; count++) {
+//             if (i % count === 0) {
+//                 break;
+//             }
+//         }
+//         if (count === num) {
+//             console.log("Число простое")
+//         }
+//     }
+//     console.log("Найменьший делитель: " + count)
+// }
+
 if (num < 1) {
     console.log("NaN")
-} else if (num === 1) {
-    console.log("Число простое")
 } else {
     for (i = 2; i <= num; i++) {
         for (count = 2; count < num; count++) {
@@ -13,9 +29,11 @@ if (num < 1) {
                 break;
             }
         }
-        if (count === num) {
-            console.log("Число простое")
-        }
     }
-    console.log("Найменьший делитель: " + count)
+}
+
+if (count === num || num === 1) {
+    console.log ("Число простое")
+}   else {
+    console.log("Наименьшей делитель числа " + num + " являеться " + count)
 }
